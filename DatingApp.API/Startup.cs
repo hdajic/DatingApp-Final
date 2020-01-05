f1316cb091ddc1eada222723dbc87a98084a9ea4
Haris <hdajic@outlook.com>
2020-01-05 23:19:40 +0100

    Finish

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -40,7 +40,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             
             IdentityBuilder builder = services.AddIdentityCore<User>(opt =>
             {
@@ -119,7 +119,7 @@ namespace DatingApp.API
                 // app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             seeder.SeedUsers();
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
